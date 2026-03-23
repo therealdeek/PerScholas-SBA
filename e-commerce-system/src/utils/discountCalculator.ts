1636088dfb5e4ce4f8340a4eb7e39fdc74f3ca0e
therealdee_k <davidkea2@gmail.com>
2026-03-23 16:55:16 -0500

    built out the discountCalculator file to calculate the dollar amount a product is discounted by

--- e-commerce-system/src/utils/discountCalculator.ts
+++ e-commerce-system/src/utils/discountCalculator.ts
@@ -1,3 +1,15 @@
 
+export function calculateDiscount (
+    price: number,
+    discountPercentage: number
+): number {
+    if (price < 0 ) {
+        throw new RangeError ("Price cannot be negative.");
+    }
+    if (discountPercentage < 0 || discountPercentage > 100) {
+        throw new RangeError ("Discount percentage must be between 0 and 100.");
+    }
+    return price * (discountPercentage / 100);
+}
 
 
